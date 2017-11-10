428a5d031f3495478d586b2989d6f6349a425203
Setoa <diqzk528@naver.com>
2017-11-11 04:23:11 +0900

    background processing, not fg. socket, not IPC

---
+++ mysh-1/src/signal_handlers.c
@@ -0,0 +1,16 @@
+#include "signal_handlers.h"
+#include <signal.h>
+#include <unistd.h>
+void catch_sigint(int signalNo)
+{
+  // TODO: File this!
+  signal(SIGINT,SIG_IGN);
+  sleep(1);
+}
+
+void catch_sigtstp(int signalNo)
+{
+  // TODO: File this!
+  signal(SIGTSTP,SIG_IGN);
+  sleep(1);
+}
